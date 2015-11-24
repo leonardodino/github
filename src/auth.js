1b2ac598fda9015dbca0e86f648decb09293a8d8
Leonardo Dino <leonardo@kunstdesign.com.br>
2015-11-24 21:35:23 -0200

    code style

--- src/auth.js
+++ src/auth.js
@@ -14,23 +14,23 @@ class GithubLoginService{
 		this._loadState()
 		return this;
 	}
-	
+
 	_setState(newState){
 		this.state = merge(this.state, newState)
 		localStorage.setItem(LSKey, JSON.stringify(this.state))
 	}
-	
+
 	isAuthenticated(){
 		return this.state.authenticated;
 	}
-	
+
 	login(){
 		if(this.isAuthenticated()){
 			return Promise.resolve({token: this.state.token})
-			
+
 		}else if(!this.state.error){
 			this._checkCode()
-			
+
 			if(!this.state.code){
 				this._requestAuthDialog()
 				return new Promise(noop)
@@ -41,12 +41,12 @@ class GithubLoginService{
 		}
 		return Promise.reject(this.state.error)
 	}
-	
+
 	logout(){
 		this._resetState();
 		return Promise.resolve({})
 	}
-	
+
 	_getCodeFromUrl(){
 		var query = window.location.search.replace(/^\?/, '');
 		if(query.indexOf('code=') > -1){
@@ -54,7 +54,7 @@ class GithubLoginService{
 		}
 		return (!!code && code != 'undefined') && code;
 	}
-	
+
 	_checkCode(){
 		var code = this._getCodeFromUrl()
 		if(code){
@@ -70,21 +70,21 @@ class GithubLoginService{
 			.then((data)=>this._authenticate(data))
 			.catch((error)=>this._authenticateError(error))
 	}
+
 	_resetState(){
 		localStorage.removeItem(LSKey);
 		this.constructor();
 	}
+
 	_loadState(){
 		var snapshot = localStorage.getItem(LSKey, this.state);
-		if(snapshot){
-			this.state = JSON.parse(snapshot)
-		}
+		this.state = snapshot ? JSON.parse(snapshot) : this.state
 	}
 
 	_requestAuthDialog(){
 		window.location = "https://github.com/login/oauth/authorize?client_id=7422da94fba5f7b84e7d&scope=repo";
 	}
-	
+
 	_authenticate({token}){
 		if(token){
 			this._setState({token, authenticated: true})
@@ -93,6 +93,7 @@ class GithubLoginService{
 			return Promise.reject(new Error('authorization token missing'))
 		}
 	}
+
 	_authenticateError(error){
 		this._setState({error})
 		return error;
